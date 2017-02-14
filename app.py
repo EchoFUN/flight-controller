a8bb627b5ce674def0439a01d57fb3ba9c326a4f
EchoFUN <xukai.ken@gmail.com>
2017-02-14 14:35:47 +0800

    Add

--- app.py
+++ app.py
@@ -13,39 +13,23 @@
 # 
 
 import sys
-import termios
-import contextlib
+import time
 
-import os
-
-
-@contextlib.contextmanager
-def raw_mode(file):
-    old_attrs = termios.tcgetattr(file.fileno())
-    new_attrs = old_attrs[:]
-    new_attrs[3] = new_attrs[3] & ~(termios.ECHO | termios.ICANON)
-    try:
-        termios.tcsetattr(file.fileno(), termios.TCSADRAIN, new_attrs)
-        yield
-    finally:
-        termios.tcsetattr(file.fileno(), termios.TCSADRAIN, old_attrs)
+import readchar
 
 
-def main():
-    print 'exit with ^C or ^D'
-    with raw_mode(sys.stdin):
-        try:
-            while True:
-                ch = sys.stdin.read(1)
-                if not ch or ch == chr(4):
-                    break
-                # print '%02x,hi' % ord(ch),
+def readerEvent():
 
-                os.system('clear')
+    while True:
+        key_code = repr(readchar.readkey())
+        print(key_code)
 
-        except (KeyboardInterrupt, EOFError):
-            pass
+        
+
+        # Exit when needed.
+        if key_code == "'\\x7f'":
+            sys.exit('Exit the controller.')
 
 
 if __name__ == '__main__':
-    main()
+    readerEvent()
--- directives/lock.py
+++ directives/lock.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 
-#
+# 
 # @author XU Kai(xukai.ken@gmail.com)
 # @date 2017-02-12 星期日
-#
-#
+# 
+# 
 # 
 # @fileOverview 解锁 APM 飞行控制器
-#
-#
+# 
+# 
 # 
 
 import time
