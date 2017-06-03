7156bae4a0a15bda7ca6cf1ed020275b590f2751
EchoFUN <xukai.ken@gmail.com>
2017-06-03 21:33:34 +0800

    Under Constructiom .

---
+++ tests/usb.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+#
+# @author XU Kai(xukai.ken@gmail.com)
+# @date 2017-02-12 星期日
+#
+#
+# 
+# @fileOverview The controller .
+#
+#
+# 
+
+import glob
+import serial
+
+
+def serial_ports():
+    ports = glob.glob('/dev/tty*')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+if __name__ == '__main__':
+    print(serial_ports())
